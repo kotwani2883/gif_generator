9b92395469ce47c1b4537151057db17d131ebda2
Palak <palakkotwani2883@gmail.com>
2021-01-02 22:06:32 +0530

    Initial Commit

---
+++ src/components/Tag_V1.jsx
@@ -0,0 +1,39 @@
+import React,{useState,useEffect} from 'react'
+import axios from 'axios'
+const API_KEY=process.env.REACT_APP_API_KEY;
+
+const Tag=()=>{
+    const [tag,setTag]=useState('dogs');
+const [gif,setGif]=useState('');
+
+
+
+const fetchGif=async(tag)=>{
+    const url = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+   const {data}=await axios.get(url);//promise
+const imageSrc=data.data.images.downsized_large.url;
+
+setGif(imageSrc)
+
+}
+
+
+    //COMPONENT DID MOUNT....first render
+    useEffect(()=>{
+        fetchGif(tag);
+    },[]);
+
+    const handleClick=()=>{
+fetchGif(tag);
+    }
+    return (
+        <div className="container">
+            <h1>Random {tag} Gif</h1>
+            <img width="500"  src={gif} alt="Random gif"/>
+            <input  value={tag} onChange={(e)=>setTag(e.target.value)}/>
+            <button onClick={handleClick}>Click for New</button>
+        </div>
+    );
+}
+
+export default Tag;
